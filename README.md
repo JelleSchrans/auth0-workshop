5ec77e2c4b5c1caa78df3ec311483faa674e4d4e
unknown <cja.obermeier@student.han.nl>
2024-05-07 14:20:34 +0200

    Backend staat klaar met ingevulde README hiervoor.

--- README.md
+++ README.md
@@ -27,4 +27,14 @@ Dit zijn alle dependencies die nodig zijn voor backend en frontend van de demo v
 
 - [Microsoft.AspNetCore.Authentication.JwtBearer](https://www.nuget.org/packages/Microsoft.AspNetCore.Authentication.JwtBearer)
 
-## 
+## Frontend
+
+1. benoem hier stappen...
+
+## Backend
+
+1. Als het inloggen werkt, dan kan er in de `Program.cs` de authentication en authorization uitgecomment worden.
+
+2. Voeg in de `appsettings.json` de juiste data in die je op auth0 hebt gebruikt: domein & audience.
+
+3. Ga naar de controller en comment de authorization annotaties uit.
---
+++ backend/Controllers/Auth0Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auth0Controller : ControllerBase
+    {
+        [HttpGet("/authentication")]
+        /*[Authorize]*/
+        public ActionResult<string> GetAuthentication()
+        {
+            const string authenticated = "You have been authenticated!";
+            return Ok(authenticated);
+        }
+
+        [HttpGet("/authorization")]
+        /*[Authorize("read:authorized")]*/
+        public ActionResult<string> GetAuthorization()
+        {
+            const string authorized = "You have been authorized!";
+            return Ok(authorized);
+        }
+    }
+}
